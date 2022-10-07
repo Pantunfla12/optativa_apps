1b09f3e9e4c019b622b858449a0ebbd38a80ce70
pantunfla12 <jmorales28@ucol.mx>
2022-10-06 19:06:03 -0500

    actividad6

---
+++ p1/actividad6/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Login from "./views/Login";
+import SignUp from "./views/SignUp";
+import HomeScreen from "./views/HomeScreen";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+};
